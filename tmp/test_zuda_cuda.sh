3ea4c730a84553a87d90e3ae5a44a38292912fba
zhangxin <zhangxin8069@qq.com>
2023-08-03 00:24:49 +0800

    update

--- tmp/test_zuda_cuda.sh
+++ tmp/test_zuda_cuda.sh
@@ -1,3 +1,3 @@
 pushd /public/home/zhangxin/zuda/tmp
-nvcc -O3 -arch=sm_86 -o test_zuda_cuda /public/home/zhangxin/zuda/test/test_zuda_cuda.cu && nvprof ./test_zuda_cuda
+nvcc -O3 -arch=sm_86 -o ./test_zuda_cuda /public/home/zhangxin/zuda/test/test_zuda_cuda.cu -g -G && nsys profile --stats=true ./test_zuda_cuda
 popd
